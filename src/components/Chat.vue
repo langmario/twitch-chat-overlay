dedfa66ec16e836de4ad89b1de3c1bca3acbab12
Mario Lang <mario@mlwx.de>
2026-05-29 09:16:03 +0000

    feat: chat scroll animation

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -5,19 +5,25 @@ import ChatMessage from './ChatMessage.vue'
 
 const { channel } = defineProps<{ channel: string }>()
 
-const { messages, isConnected } = useChat(channel, 50)
+const { messages, isConnected } = useChat(channel, 20)
 </script>
 
 <template>
   <div class="relative size-full overflow-hidden">
-    <ul
+    <TransitionGroup
+      tag="ul"
+      move-class="transition-all duration-500 ease-out-expo"
+      enter-active-class="transition-all duration-500 ease-out-expo"
+      leave-active-class="transition-all duration-500 ease-out-expo absolute"
+      enter-from-class="opacity-0 translate-y-full"
+      leave-to-class="opacity-0"
       class="scrollbar-none flex size-full flex-col justify-end gap-0.5 overflow-scroll scroll-smooth py-2 leading-snug text-white transition-opacity"
       :class="{
         'opacity-50': !isConnected,
       }"
     >
       <ChatMessage v-for="message of messages" :key="message.id" v-bind="message" />
-    </ul>
+    </TransitionGroup>
     <Transition
       enter-active-class="transition ease-out"
       leave-active-class="transition ease-out"
