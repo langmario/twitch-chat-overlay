6dc3184b6d792281056a9d59715692c5689e9b7b
Mario Lang <mario@mlwx.de>
2026-06-02 14:41:45 +0000

    fix: spacing

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -20,27 +20,20 @@ const { user, text, emoteOffsets } = defineProps<Message & { sevenTvEmotes?: Emo
     :data-first="flags?.isFirst"
     :data-deleted="deleted"
     :style="{ '--user-color': user.color }"
-    class="group/message relative isolate overflow-clip rounded px-3 py-0.5 transition will-change-transform data-[broadcaster=true]:bg-broadcaster/10 data-[first=true]:bg-blue-300/10 data-[mod=true]:bg-mod/10 data-[vip=true]:bg-vip/10"
+    class="group/message relative isolate overflow-clip rounded px-2 py-0.5 transition will-change-transform data-[broadcaster=true]:bg-broadcaster/10 data-[first=true]:bg-blue-300/10 data-[mod=true]:bg-mod/10 data-[vip=true]:bg-vip/10"
   >
-    <div
-      class="absolute inset-y-0 left-0 w-0.5 group-data-[broadcaster=true]/message:bg-broadcaster group-data-[mod=true]/message:bg-mod group-data-[vip=true]/message:bg-vip"
-    ></div>
     <div v-if="flags?.isFirst" class="text-xs text-blue-500 uppercase italic">First message</div>
     <div v-if="parent" class="flex gap-0.5 truncate text-sm opacity-50">
       <IconReply class="shrink-0 -scale-x-100" />
       <span class="truncate pr-0.5 italic">{{ parent.text }}</span>
     </div>
     <div>
-      <div class="inline-block">
-        <img
-          v-if="flags?.isBroadcaster"
-          :src="IconBroadcaster"
-          class="mr-1.5 inline h-lh align-middle"
-        />
-        <img v-else-if="flags?.isMod" :src="IconMod" class="mr-1.5 inline h-lh align-middle" />
-        <img v-else-if="flags?.isVIP" :src="IconVIP" class="mr-1.5 inline h-lh align-middle" />
+      <div class="mr-1.5 inline-block">
+        <img v-if="flags?.isBroadcaster" :src="IconBroadcaster" class="mr-1.5 inline h-lh" />
+        <img v-else-if="flags?.isMod" :src="IconMod" class="mr-1.5 inline h-lh" />
+        <img v-else-if="flags?.isVIP" :src="IconVIP" class="mr-1.5 inline h-lh" />
         <span id="username" class="font-bold">{{ user.name }}</span>
-        <span class="mr-1">:</span>
+        <span>:</span>
       </div>
       <div class="inline group-data-[deleted=true]/message:blur-sm">
         <FormattedMessage
